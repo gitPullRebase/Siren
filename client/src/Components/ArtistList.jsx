8cfdf5f45478218e9002ff5512e099d234c5f3f9
David Arellano <david@davids-mbp.attlocal.net>
2017-11-21 12:53:07 -0800

    changed list to ordered list

--- client/src/Components/ArtistList.jsx
+++ client/src/Components/ArtistList.jsx
@@ -5,17 +5,19 @@ import Artist from "./Artist.jsx";
 const ArtistList = props => (
   <div className="col artistList">
     <h3>Artists Available in SF</h3>
-    {props.artists.map((artist, index) => {
-      return (
-        <div>
-          <label>
-            <strong>{index + 1 + "."}</strong>
-          </label>
-          <Artist artist={artist} />
-          <br />
-        </div>
-      );
-    })}
+    <ol>
+      {props.artists.map((artist, index) => {
+        return (
+          <div>
+            <li>
+              <Artist artist={artist}
+                      index={index} />
+            </li>
+            <br />
+          </div>
+        );
+      })}
+    </ol>
   </div>
 );
 
