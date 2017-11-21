40b884a7b4db59cfb9528643493c151cb667a959
David Arellano <david@davids-mbp.attlocal.net>
2017-11-21 08:21:02 -0800

    setting up pull

--- client/src/Components/Song.jsx
+++ client/src/Components/Song.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Song = () => (
   <div>
-    <iframe src="https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:3rgsDhGHZxZ9sB9DQWQfuf" width="300" height="380" frameBorder="0" allowTransparency="true"></iframe>
-    <iframe src="https://open.spotify.com/embed?uri=spotify:track:5JunxkcjfCYcY7xJ29tLai"
+    <iframe src="https://open.spotify.com/embed?uri=spotify:track:1W4CZ57WzY1w8fVH53hD4L"
         frameBorder="0" allowTransparency="true"></iframe>
   </div>
 )
