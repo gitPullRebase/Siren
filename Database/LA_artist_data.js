234c9584ff4392c310b6a7bed70c3e23bd8c7736
Edwin Chu <edwinychu@gmail.com>
2017-11-21 12:21:16 -0800

    new data files for LA artists and NY artists

---
+++ Database/LA_artist_data.js
@@ -0,0 +1,345 @@
+module.exports = [
+  {
+    external_urls: {
+      spotify: "https://open.spotify.com/artist/7LTShHcq1KdTrWeLvWoYed"
+    },
+    followers: { href: null, total: 66061 },
+    genres: ["k-hop", "underground hip hop"],
+    href: "https://api.spotify.com/v1/artists/7LTShHcq1KdTrWeLvWoYed",
+    id: "7LTShHcq1KdTrWeLvWoYed",
+    images: [
+      {
+        height: 640,
+        url: "https://i.scdn.co/image/4e9029bb24f55c60216a7076574ce619d0d16775",
+        width: 640
+      },
+      {
+        height: 320,
+        url: "https://i.scdn.co/image/2d42a4be32d338906ac1d27d4360877fc0cae924",
+        width: 320
+      },
+      {
+        height: 160,
+        url: "https://i.scdn.co/image/cef6aaefeb4538c394da5e89df30eb22ce416aa0",
+        width: 160
+      }
+    ],
+    name: "Dumbfoundead",
+    popularity: 61,
+    type: "artist",
+    uri: "spotify:artist:7LTShHcq1KdTrWeLvWoYed"
+  },
+  {
+    external_urls: {
+      spotify: "https://open.spotify.com/artist/77AiFEVeAVj2ORpC85QVJs"
+    },
+    followers: { href: null, total: 1633887 },
+    genres: ["big room", "dance pop", "edm", "electro house", "tropical house"],
+    href: "https://api.spotify.com/v1/artists/77AiFEVeAVj2ORpC85QVJs",
+    id: "77AiFEVeAVj2ORpC85QVJs",
+    images: [
+      {
+        height: 640,
+        url: "https://i.scdn.co/image/9fb14e9cb53a341db6c4ef132e0c01f9c9a1dd40",
+        width: 640
+      },
+      {
+        height: 320,
+        url: "https://i.scdn.co/image/c3d56a5a59dc06938b3740456349fceffb172c02",
+        width: 320
+      },
+      {
+        height: 160,
+        url: "https://i.scdn.co/image/8c76f0734ba27e5e33bf38984f0301494983f431",
+        width: 160
+      }
+    ],
+    name: "Steve Aoki",
+    popularity: 81,
+    type: "artist",
+    uri: "spotify:artist:77AiFEVeAVj2ORpC85QVJs"
+  },
+  {
+    external_urls: {
+      spotify: "https://open.spotify.com/artist/5fMUXHkw8R8eOP2RNVYEZX"
+    },
+    followers: { href: null, total: 1095673 },
+    genres: [
+      "bmore",
+      "brostep",
+      "edm",
+      "electro house",
+      "electronic trap",
+      "moombahton",
+      "ninja"
+    ],
+    href: "https://api.spotify.com/v1/artists/5fMUXHkw8R8eOP2RNVYEZX",
+    id: "5fMUXHkw8R8eOP2RNVYEZX",
+    images: [
+      {
+        height: 640,
+        url: "https://i.scdn.co/image/72cf22afd023ad548d8b073fdfaefb799f929cca",
+        width: 640
+      },
+      {
+        height: 320,
+        url: "https://i.scdn.co/image/e624f08da12c536413640d43bb2cc6881bf9c6f7",
+        width: 320
+      },
+      {
+        height: 160,
+        url: "https://i.scdn.co/image/28daa9aabdf650999baa0ea6f1bccd8b3d160bd1",
+        width: 160
+      }
+    ],
+    name: "Diplo",
+    popularity: 80,
+    type: "artist",
+    uri: "spotify:artist:5fMUXHkw8R8eOP2RNVYEZX"
+  },
+  {
+    external_urls: {
+      spotify: "https://open.spotify.com/artist/5he5w2lnU9x7JFhnwcekXX"
+    },
+    followers: { href: null, total: 5359946 },
+    genres: ["brostep", "complextro", "edm", "electro", "electro house"],
+    href: "https://api.spotify.com/v1/artists/5he5w2lnU9x7JFhnwcekXX",
+    id: "5he5w2lnU9x7JFhnwcekXX",
+    images: [
+      {
+        height: 640,
+        url: "https://i.scdn.co/image/dfc464638a476db61668e4f0d0e028226530b92f",
+        width: 640
+      },
+      {
+        height: 320,
+        url: "https://i.scdn.co/image/72ab5780d4ff00545135320a8a926f24b66a1572",
+        width: 320
+      },
+      {
+        height: 160,
+        url: "https://i.scdn.co/image/b503d38ef805d6f3ff618558106f82234710875b",
+        width: 160
+      }
+    ],
+    name: "Skrillex",
+    popularity: 87,
+    type: "artist",
+    uri: "spotify:artist:5he5w2lnU9x7JFhnwcekXX"
+  },
+  {
+    external_urls: {
+      spotify: "https://open.spotify.com/artist/7wg1qvie3KqDNQbAkTdbX0"
+    },
+    followers: { href: null, total: 123745 },
+    genres: [
+      "edm",
+      "electronic trap",
+      "indie poptimism",
+      "tropical house",
+      "vapor soul",
+      "vapor twitch"
+    ],
+    href: "https://api.spotify.com/v1/artists/7wg1qvie3KqDNQbAkTdbX0",
+    id: "7wg1qvie3KqDNQbAkTdbX0",
+    images: [
+      {
+        height: 640,
+        url: "https://i.scdn.co/image/c54aeee5871632de61735ae7ed53e07cdb45ef70",
+        width: 640
+      },
+      {
+        height: 320,
+        url: "https://i.scdn.co/image/289727cbf7e8d832e5f0002fac63b651e059f5ea",
+        width: 320
+      },
+      {
+        height: 160,
+        url: "https://i.scdn.co/image/710a53dfcbdea3e704429950bfca71c531e519e9",
+        width: 160
+      }
+    ],
+    name: "Louis The Child",
+    popularity: 72,
+    type: "artist",
+    uri: "spotify:artist:7wg1qvie3KqDNQbAkTdbX0"
+  },
+  {
+    external_urls: {
+      spotify: "https://open.spotify.com/artist/0cmWgDlu9CwTgxPhf403hb"
+    },
+    followers: { href: null, total: 666072 },
+    genres: [
+      "downtempo",
+      "electronic",
+      "indie r&b",
+      "indietronica",
+      "ninja",
+      "nu jazz",
+      "trip hop"
+    ],
+    href: "https://api.spotify.com/v1/artists/0cmWgDlu9CwTgxPhf403hb",
+    id: "0cmWgDlu9CwTgxPhf403hb",
+    images: [
+      {
+        height: 640,
+        url: "https://i.scdn.co/image/8822cefd38100390e68c77481d4d2cd90f74bb32",
+        width: 640
+      },
+      {
+        height: 320,
+        url: "https://i.scdn.co/image/ec2d5e7c1724ce36b6a5e732150ceb43568dd557",
+        width: 320
+      },
+      {
+        height: 160,
+        url: "https://i.scdn.co/image/3441501caa35623c8f5f6d87bd4eb22823ce53f7",
+        width: 160
+      }
+    ],
+    name: "Bonobo",
+    popularity: 74,
+    type: "artist",
+    uri: "spotify:artist:0cmWgDlu9CwTgxPhf403hb"
+  },
+  {
+    external_urls: {
+      spotify: "https://open.spotify.com/artist/137W8MRPWKqSmrBGDBFSop"
+    },
+    followers: { href: null, total: 5885955 },
+    genres: ["pop rap", "rap", "southern hip hop", "trap music"],
+    href: "https://api.spotify.com/v1/artists/137W8MRPWKqSmrBGDBFSop",
+    id: "137W8MRPWKqSmrBGDBFSop",
+    images: [
+      {
+        height: 640,
+        url: "https://i.scdn.co/image/52465652af91d7671c92b4b7fd49bc7470b95e38",
+        width: 640
+      },
+      {
+        height: 320,
+        url: "https://i.scdn.co/image/c1823ac84c4101c0dffaac562c03e739cdf2df75",
+        width: 320
+      },
+      {
+        height: 160,
+        url: "https://i.scdn.co/image/a4c3dc3a4d2b3a9ed6b0ced6d4a969789971ac7d",
+        width: 160
+      }
+    ],
+    name: "Wiz Khalifa",
+    popularity: 91,
+    type: "artist",
+    uri: "spotify:artist:137W8MRPWKqSmrBGDBFSop"
+  },
+  {
+    external_urls: {
+      spotify: "https://open.spotify.com/artist/1PMiXPKjsgXEDDcfIRNVaq"
+    },
+    followers: { href: null, total: 3364 },
+    genres: [],
+    href: "https://api.spotify.com/v1/artists/1PMiXPKjsgXEDDcfIRNVaq",
+    id: "1PMiXPKjsgXEDDcfIRNVaq",
+    images: [
+      {
+        height: 640,
+        url: "https://i.scdn.co/image/a78ff689f455ed0b499866edb776f6e757bf41fb",
+        width: 640
+      },
+      {
+        height: 320,
+        url: "https://i.scdn.co/image/64dae2f622b0711fdea5950ef0ed86eb562707db",
+        width: 320
+      },
+      {
+        height: 160,
+        url: "https://i.scdn.co/image/68bcbe3ed94a191df2faaf2c96237f34c6f1db75",
+        width: 160
+      }
+    ],
+    name: "Matt DiMona",
+    popularity: 49,
+    type: "artist",
+    uri: "spotify:artist:1PMiXPKjsgXEDDcfIRNVaq"
+  },
+  {
+    external_urls: {
+      spotify: "https://open.spotify.com/artist/5ttgIeUVka6FLyi00Uu5h8"
+    },
+    followers: { href: null, total: 161156 },
+    genres: [
+      "bass trap",
+      "big room",
+      "brostep",
+      "catstep",
+      "deep big room",
+      "deep groove house",
+      "edm",
+      "electro house",
+      "electronic trap",
+      "house",
+      "moombahton",
+      "progressive electro house"
+    ],
+    href: "https://api.spotify.com/v1/artists/5ttgIeUVka6FLyi00Uu5h8",
+    id: "5ttgIeUVka6FLyi00Uu5h8",
+    images: [
+      {
+        height: 640,
+        url: "https://i.scdn.co/image/3204890bc6320c4dce1bcf729302fae25f8651c2",
+        width: 640
+      },
+      {
+        height: 320,
+        url: "https://i.scdn.co/image/ef68a1d0c13bff1a04823fa1b2f64bd46e9c5e8c",
+        width: 320
+      },
+      {
+        height: 160,
+        url: "https://i.scdn.co/image/e273c8faa365807922d351ecacc0c3b0eb11c110",
+        width: 160
+      }
+    ],
+    name: "Jauz",
+    popularity: 68,
+    type: "artist",
+    uri: "spotify:artist:5ttgIeUVka6FLyi00Uu5h8"
+  },
+  {
+    external_urls: {
+      spotify: "https://open.spotify.com/artist/2ZRQcIgzPCVaT9XKhXZIzh"
+    },
+    followers: { href: null, total: 73644 },
+    genres: [
+      "big room",
+      "deep tropical house",
+      "edm",
+      "electro house",
+      "indie poptimism",
+      "tropical house"
+    ],
+    href: "https://api.spotify.com/v1/artists/2ZRQcIgzPCVaT9XKhXZIzh",
+    id: "2ZRQcIgzPCVaT9XKhXZIzh",
+    images: [
+      {
+        height: 640,
+        url: "https://i.scdn.co/image/3cc163f6e55db9c41a13b8d6ae222e27a8852904",
+        width: 640
+      },
+      {
+        height: 320,
+        url: "https://i.scdn.co/image/df2ba9e210aa50743b8c4728a0d329fc0349324e",
+        width: 320
+      },
+      {
+        height: 160,
+        url: "https://i.scdn.co/image/883bfaf16494266143f52925ceebdb85b83b7a35",
+        width: 160
+      }
+    ],
+    name: "Gryffin",
+    popularity: 78,
+    type: "artist",
+    uri: "spotify:artist:2ZRQcIgzPCVaT9XKhXZIzh"
+  }
+];
