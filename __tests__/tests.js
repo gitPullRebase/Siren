2b4302319470e5b8531630921abc1939650e748c
Edwin Chu <edwinychu@gmail.com>
2017-11-21 16:45:17 -0800

    setting up our testing suite with Jest

---
+++ __tests__/tests.js
@@ -0,0 +1,19 @@
+/*  Jest is really FREAKING AWESOME because it comes with everything 
+packaged together. This means no more extra libraries for testing. Jest
+also allows for both front-end (only React though I believe) and 
+back-end testing. Really makes writing unit tests for your particular
+feature easy!
+
+Refer below for writing correct tests:
+
+1. Create a new section for your tests using comments 
+  (ex: //Tests for Artist Component & Artist List Component)
+
+2. Write 5 tests that pertain to only YOUR ticket 
+
+3. Don't worry about other people's tests that are already in here, just 
+keep adding yours to the top
+
+*/
+
+//1. Tests for Artist Componenet & Artist List Component
