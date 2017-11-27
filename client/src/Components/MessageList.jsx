073dab54c259e8eacf73beb21900ca26acbe30f4
Edwin Chu <edwinychu@gmail.com>
2017-11-27 14:21:30 -0800

    final refactoring

--- client/src/Components/MessageList.jsx
+++ client/src/Components/MessageList.jsx
@@ -7,20 +7,19 @@ const MessageList = props => {
   let chatrooms = props.chatrooms;
   let isArtist = props.isArtist;
   if (isArtist) {
-
     return (
       <div>
-        <h2>Artist Messages</h2>
+        <h2>Gig Requests</h2>
         {chatrooms.map((chatroom, index) => {
           return (
             <div key={index}>
-                <Message
-                  isArtist={props.isArtist}
-                  chatroom={chatroom}
-                  messageClickHandler={props.messageClickHandler}
-                  declineClickHandler={props.declineClickHandler}
-                  acceptClickHandler={props.acceptClickHandler}
-                />
+              <Message
+                isArtist={props.isArtist}
+                chatroom={chatroom}
+                messageClickHandler={props.messageClickHandler}
+                declineClickHandler={props.declineClickHandler}
+                acceptClickHandler={props.acceptClickHandler}
+              />
             </div>
           );
         })}
@@ -29,15 +28,15 @@ const MessageList = props => {
   } else {
     return (
       <div className="container">
-        <h2 className="text-center">User Message</h2>
+        <h2 className="text-center">Your Requests</h2>
         {chatrooms.map((chatroom, index) => {
           return (
             <div key={index}>
-                <Message
-                  isArtist={props.isArtist}
-                  chatroom={chatroom}
-                  messageClickHandler={props.messageClickHandler}
-                />
+              <Message
+                isArtist={props.isArtist}
+                chatroom={chatroom}
+                messageClickHandler={props.messageClickHandler}
+              />
             </div>
           );
         })}
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -13,20 +13,23 @@ const Navbar = props => {
   if (props.route !== "") {
     login = "";
     profile = (
-      <Link to={{ pathname: props.route}}>
-        <a href="#" className="profileBtn">
-          Profile
-        </a>
+      <Link to={{ pathname: props.route }}>
+        <div className="profileBtn-toolbar">
+          <a href="#" className="profileBtn">
+            Profile
+          </a>
+        </div>
       </Link>
     );
   }
 
-
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Siren.
-      </a>
+      <Link to="/">
+        <a className="navbar-brand" href="#">
+          Siren.
+        </a>
+      </Link>
       <ul className="navbar-nav mr-auto">
         {profile}
         {login}
