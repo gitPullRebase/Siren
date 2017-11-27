658ebd146fdc3d97bb58e5532d82396bd8993ac7
Edwin Chu <edwinychu@gmail.com>
2017-11-27 13:28:29 -0800

    fixed prod test

--- knexfile.js
+++ knexfile.js
@@ -2,16 +2,16 @@ var environment = process.env.NODE_ENV || "development";
 //const dbPassword = require("./config/config.js").dbPassword;
 
 var config = {
-  development: {
-    client: "pg",
-    connection: `postgres://postgres:${dbPassword}@localhost:5432/artists`,
-    migrations: {
-      directory: __dirname + "/Database/migrations"
-    },
-    seeds: {
-      directory: __dirname + "/Database/seeds"
-    }
-  },
+  // development: {
+  //   client: "pg",
+  //   connection: `postgres://postgres:${dbPassword}@localhost:5432/artists`,
+  //   migrations: {
+  //     directory: __dirname + "/Database/migrations"
+  //   },
+  //   seeds: {
+  //     directory: __dirname + "/Database/seeds"
+  //   }
+  // },
   production: {
     client: "pg",
     connection: `${process.env.DATABASE_URL}`,
