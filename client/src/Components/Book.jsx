f67a60fe3caa343da1ca7886b63010ba0913d919
Jeff Tsai <jefftsai0216@gmail.com>
2017-11-30 17:20:19 -0800

    adjusted user message interface

--- client/src/Components/Book.jsx
+++ client/src/Components/Book.jsx
@@ -1,12 +1,72 @@
-import React from "react";
-import ReactModal from "react-modal";
+import React from 'react';
+import ReactModal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '75%',
+    left: '80%',
+    right: 'auto',
+    bottom: 'auto',
+    position: 'fixed',
+    transform: 'translate(-50%, -50%)',
+  },
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(255, 255, 255, 0)',
+  },
+};
 
 class Book extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      input: '',
+      msgStat: '',
+    };
   }
-
+  change(e) {
+    this.setState({
+      input: e.target.value,
+    });
+    console.log('state in book', this.state.input);
+  }
+  onSubmit() {
+    if (this.state.input === '') {
+      this.setState({
+        msgStat: 'no message',
+      });
+    } else {
+      let input = this.state.input;
+      this.setState({
+        input: '',
+        msgStat: 'success',
+      });
+      console.log('this is current state', this.state);
+      this.props.onClick(input);
+    }
+  }
+  toggleHideen() {
+    this.set;
+  }
   render() {
+    let message;
+    if (this.state.msgStat === 'success') {
+      message = (
+        <p className="book message" id="message">
+          Message has been sent!
+        </p>
+      );
+    } else if (this.state.msgStat === 'no message') {
+      message = (
+        <p className="book message" id="errmessage">
+          Message is empty!
+        </p>
+      );
+    }
     return (
       <div>
         <ReactModal
@@ -14,13 +74,28 @@ class Book extends React.Component {
           onRequestClose={this.props.handleCloseModal}
           closeOnEscape={true}
           contentLabel="Book Now Modal"
+          style={customStyles}
         >
-          <input ref={input => (this._message = input)} />
-          <button onClick={() => this.props.onClick(this._message.value)}>
-            {" "}
-            Send{" "}
-          </button>
-          <button onClick={this.props.handleCloseModal}>Cancel</button>
+          <h4 className="book message">To {this.props.artist}</h4>
+          <textarea
+            id="book-message-textbox"
+            color="black"
+            rows="9"
+            cols="50"
+            placeholder="Please enter your message here"
+            value={this.state.input}
+            onChange={this.change.bind(this)}
+          />
+          <div>
+            <button className="btn btn-primary" onClick={this.onSubmit.bind(this)}>
+              {' '}
+              Send{' '}
+            </button>
+            <button className="btn btn-primary" onClick={this.props.handleCloseModal}>
+              Cancel
+            </button>
+            {message}
+          </div>
         </ReactModal>
       </div>
     );
@@ -28,4 +103,3 @@ class Book extends React.Component {
 }
 
 export default Book;
-
